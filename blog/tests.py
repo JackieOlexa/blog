3e906e2fa275666bb1a22d74ecf8cb7109f60236
JackieOlexa <jackie.olexa@umconnect.umt.edu>
2023-10-12 11:27:06 -0600

    created new post functionality

--- blog/tests.py
+++ blog/tests.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from .models import Post
 
 # Create your tests here.
+
 class BlogTests(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -23,7 +24,8 @@ class BlogTests(TestCase):
         self.assertEqual(self.post.body, "Nice body content")
         self.assertEqual(self.post.author.username, "testuser")
         self.assertEqual(str(self.post), "A good title")
-        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+
 
     def test_url_exists_at_correct_location_listview(self):
         response = self.client.get("/")
@@ -40,7 +42,8 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
     def test_post_detailview(self):
-        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+
         no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
